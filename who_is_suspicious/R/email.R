e33a796a02e387ddae234c62448431f0bfec32a5
yenngee <ngyenngee@hotmail.com>
2021-08-13 01:08:39 +0800

    finished first draft of email with basic functionality

--- who_is_suspicious/R/email.R
+++ who_is_suspicious/R/email.R
@@ -1,6 +1,9 @@
 library(tidyverse)
 
 ## Load Data ----
+
+email_headers <- read_csv("data/email/email_headers.csv")
+
 # Nodes
 gastech_nodes <- read_rds("data/email/gastech_employees.rds") %>% 
   mutate(title = paste(label, CurrentEmploymentTitle,' ')) %>%
@@ -9,16 +12,13 @@ gastech_nodes <- read_rds("data/email/gastech_employees.rds") %>%
   select(id, label, group, title, citizenship)
 
 # Edges 
-gastech_emails <- read_rds("data/email/gastech_emails.rds")
+gastech_edges <- read_rds("data/email/gastech_emails.rds")%>% 
+  rename(from = source, to = target)  %>% 
+  left_join(email_headers, by='Subject') 
 
-gastech_edges <- gastech_emails %>% 
-  group_by(source, target) %>%
-  summarize(weight=n()) %>%
-  filter(weight>1) %>%
-  ungroup() %>% 
-  rename(from = source, to = target) %>% 
-  rowid_to_column("id")
-
+# layout 
+layout_list <- c("layout_with_fr", 'layout_nicely', 'layout_with_sugiyama', "layout_in_circle")
+names(layout_list) <- c("Fruchterman Reingold", "Nicely", "Sugiyama", "Circle")
 
 ## app ----
 emailUI <- function(id) {
@@ -26,13 +26,34 @@ emailUI <- function(id) {
     sidebarLayout(
       sidebarPanel(
         selectInput(
-          inputId='dept', 
+          inputId = NS(id, 'dept'), 
           label = "Department", 
-          choices = gastech_nodes$group, 
+          choices = unique(gastech_nodes$group), 
+          selected = unique(gastech_nodes$group), 
+          multiple= TRUE
+        ),
+        selectInput(
+          inputId = NS(id, 'email_type'), 
+          label = "Email Type", 
+          choices = unique(email_headers$EmailType), 
+          multiple=FALSE
+        ),
+        sliderInput(
+          inputId = NS(id, 'freq'), 
+          label = "Weight", 
+          min = 1,
+          max = 20,
+          value = 5,
+        ),
+        selectInput(
+          inputId = NS(id, 'layout'), 
+          label = "Network Graph Layout", 
+          choices = names(layout_list), 
           multiple=FALSE
         )
       ),
       mainPanel(visNetworkOutput(NS(id, "email")))
+      
     )
   )
 }
@@ -42,13 +63,29 @@ emailServer <- function(id) {
   moduleServer(id, function(input, output, session) {
     
     output$email <- renderVisNetwork({
-    
-      visNetwork(gastech_nodes, gastech_edges, width = "100%", height = '100%') %>%
-        visIgraphLayout(layout = "layout_with_fr") %>%
+      
+      selected_nodes <- gastech_nodes %>%
+        filter(group %in% input$dept)
+      
+      selected_edges <- gastech_edges %>%
+        filter(EmailType %in% input$email_type) %>%
+        filter(from %in% selected_nodes$id) %>%
+        filter(to %in% selected_nodes$id ) %>%
+        group_by(from, to) %>%
+        summarize(weight=n()) %>%
+        filter(weight>=input$freq) %>%
+        ungroup() %>% 
+        rowid_to_column("id")
+      
+      visNetwork(selected_nodes, selected_edges, width = "100%", height = "150%") %>%
+        visIgraphLayout(layout = layout_list[[input$layout]] ) %>%
         visLegend() %>%
         visEdges(smooth = FALSE, arrows = 'to') %>%
+        visOptions(highlightNearest = list(enabled = T, degree = 1, hover = T),
+                   nodesIdSelection = TRUE) %>% 
         visLayout(randomSeed = 123)
     })
+
   })
   
 }
